0e054b5ea2ac7083a6964151dd69b57fff6a76d7
Kandelion <rlagusdn6575@gmail.com>
2018-03-27 21:37:26 +0900

    output : csv

--- web.js
+++ web.js
@@ -6,11 +6,21 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 var seq = 0
 app.get('/update', function(req, res) {
-    fs.appendFile('log.txt', JSON.stringify(req.query)+"\n", function (err){
-        if(err) throw err
-        console.log("%j", req.query)
-        res.end("Got "+ String(seq++) +" "+ JSON.stringify(req.query))
-    });
+    try{
+        let date = req.param('date');
+        let time = req.param('time');
+        let temp = req.param('temp');
+
+        const csv = date + "," + time + "," + temp; 
+        console.log(csv);
+        fs.appendFile('log.txt', String(csv)+"\n", function (err){
+            if(err) throw err
+            console.log("%j", req.query)
+            res.end("Got "+ String(seq++) +" "+ JSON.stringify(req.query))
+        });
+    }catch(err){
+        console.log(err);
+    }
 })
 
 app.get('/get', function(req, res) {
